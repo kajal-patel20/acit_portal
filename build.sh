073950c72e042a4916e733950a4add677b9e1016
Kajal Patel <kajalp2023@gmail.com>
2025-10-21 16:30:24 +0530

    Use psycopg3 instead of psycopg2-binary for better Render compatibility

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip
-pip install --upgrade pip
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
 
 # Install Python dependencies
 pip install -r requirements.txt
